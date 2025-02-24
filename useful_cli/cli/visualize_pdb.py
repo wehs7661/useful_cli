5794511818820b6f366f652d290295e125817ac8
wehs7661 <wehs7661@colorado.edu>
2025-02-24 04:59:44 +0000

    Modified visualize_pdb.py

--- useful_cli/cli/visualize_pdb.py
+++ useful_cli/cli/visualize_pdb.py
@@ -24,7 +24,7 @@ def initialize(args):
     )
     parser.add_argument(
         "-o",
-        "--output",
+        "--outputs",
         nargs="+",
         type=str,
         help="The path to the output PNG file(s). Note that if the --align_all flag is set, \
@@ -63,24 +63,56 @@ def initialize(args):
             in which case each input PDB file is aligned to the reference PDB file. \
             Note that this requires a reference PDB file to be provided with the -r flag."
     )
+    parser.add_argument(
+        "-s",
+        "--split",
+        default=False,
+        action="store_true",
+        help="Split the input PDB file into separate models/frames and align all of them to the reference structure. \
+            If the reference structure is not provided in this case, the first model will be used as the reference. \
+            Default is False."
+    )
     args = parser.parse_args()
     return args
 
-def load_and_align(pdb_file, ref=None):
+def load_and_align(obj_name, pdb_file, ref=None, split=False):
     """
     Load a PDB file into PyMOL and align it to a reference structure (if provided).
 
     Parameters
     ----------
+    obj_name : str
+        Name of the object to create in PyMOL.
     pdb_file : str
         Path to the PDB file.
     ref : str, optional
         Path to the reference PDB file. If provided, the input PDB file will be aligned to this structure.
+    split : bool, optional
+        For a PDB file containing multiple models/frames, whether to split them into separate objects
+        and align all of them to the reference structure. If the reference structure is not provided in this case,
+        the first model will be used as the reference. The default is False.
     """
     cmd = pymol.cmd
     cmd.load(pdb_file, obj_name)
-    if ref:
-        cmd.align(obj_name, ref)
+
+    if split:
+        cmd.split_states(obj_name)
+        if ref is not None:
+            cmd.load(ref, "reference")
+            for i in range(1, cmd.count_states(obj_name) + 1):
+                cmd.align(f"{obj_name}_{i:04d}", "reference")
+        else:
+            cmd.align(f"{obj_name}_0001", obj_name)
+            for i in range(2, cmd.count_states(obj_name) + 1):
+                cmd.align(f"{obj_name}_{i:04d}", f"{obj_name}_0001")
+
+        # Color the models
+        cmd.spectrum("count", "green_white_yellow", f"{obj_name}_*")
+        
+    else:
+        if ref is not None:
+            cmd.load(ref, "reference")
+            cmd.align(obj_name, "reference")
 
 
 def render_image(output, width, height, dpi):
@@ -138,10 +170,10 @@ def main():
         if args.ref:
             cmd.load(args.ref, "reference")
         for i, pdb_file in enumerate(args.pdb_files):
-            load_and_align(f"structure_{i}", pdb_file, "reference" if args.ref else None)
-        render_image(args.outputs[0])
+            load_and_align(f"structure_{i}", pdb_file, "reference" if args.ref else None, args.split)
+        render_image(args.outputs[0], args.width, args.height, args.dpi)
     else:
         for pdb_file, output in zip(args.pdb_files, args.outputs):
-            load_and_align("structure", pdb_file, args.ref)
-            render_image(output)
+            load_and_align("structure", pdb_file, args.ref, args.split)
+            render_image(output, args.width, args.height, args.dpi)
             cmd.delete("all")
