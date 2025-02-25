9fc68768e6b6ef07ecb33221996952bfecee11a9
wehs7661 <wehs7661@colorado.edu>
2025-02-25 22:41:39 +0000

    Minor tweaks

--- useful_cli/cli/combine_plots.py
+++ useful_cli/cli/combine_plots.py
@@ -11,17 +11,19 @@ def initialize():
     parser.add_argument('-f',
                         '--figs',
                         nargs='+',
+                        required=True,
                         help='The paths to the figures to be combined.')
     parser.add_argument('-d',
                         '--dimension',
                         type=int,
                         nargs='+',
-                        help='The dimensions of the subplots (n_cols, n_rows).')
+                        help='The dimensions of the subplots (n_cols, n_rows). If not specified, \
+                            the script will try to make the figure as square as possible.')
     parser.add_argument('-s',
                         '--size',
                         type=int,
                         nargs='+',
-                        help='The size of the figure (width, height).')
+                        help='The size of the figure (width, height). The default size is (6.4, 4.8).')
     parser.add_argument('-t',
                         '--titles',
                         nargs='+',
@@ -57,15 +59,14 @@ def main():
         n_cols, n_rows = utils.get_subplot_layout(len(args.figs))
     else:
         if len(args.dimension) != 2:
-            print('Warning: wrong number of arguments for specifying the dimension of the subplots.')
+            raise ValueError('The number of dimensions should be 2.')
         else:
             n_cols = args.dimension[0]
             n_rows = args.dimension[1]
 
     if args.titles is not None:
         if len(args.figs) != len(args.titles):
-            print('Error: The number of titles does not match the number of subplots.')
-            sys.exit()
+            raise ValueError('The number of titles should match the number of figures.')
 
     for i in range(len(args.figs)):
         image = cv2.imread(args.figs[i], cv2.IMREAD_COLOR)
